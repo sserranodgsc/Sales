7dd3ef012a4a91b5c02c25108ab404fd9fd97c26
sserrano <sserrano@dgsc.go.cr>
2024-03-14 10:10:02 -0600

    Create ProvinciaIndex

---
+++ Sales.WEB/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+
+namespace Sales.WEB.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public bool Error { get; set; }
+
+        public T? Response { get; set; }
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if(!Error)
+            {
+                return null;
+            }
+
+            var codigoStatus = HttpResponseMessage.StatusCode;
+
+            if (codigoStatus == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado";
+            }
+            else if (codigoStatus == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            else if(codigoStatus == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que logearte para hacer esta operación";
+            }
+            else if (codigoStatus == HttpStatusCode.Forbidden)
+            {
+                return "No tienes permisoso para hacer esta operación";
+            }
+
+            return "Ha ocurrido un error inesperado";
+        }
+    }
+}
