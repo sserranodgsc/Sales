9497622cbe54a2f5d3a1dbce9775af5e4d77b2aa
sserrano <sserrano@dgsc.go.cr>
2024-03-14 11:17:12 -0600

    Add CreateProvincias

--- Sales.API/Controllers/ProvinciasController.cs
+++ Sales.API/Controllers/ProvinciasController.cs
@@ -22,12 +22,47 @@ namespace Sales.API.Controllers
             return Ok(await _context.Provincias.ToListAsync());
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var provincia = await _context.Provincias.FirstOrDefaultAsync(p => p.Id == id);
+            if (provincia is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(provincia);
+        }
+
         [HttpPost]
         public async Task<ActionResult> Post(Provincia provincia)
         {
             _context.Add(provincia);
             await _context.SaveChangesAsync();
             return Ok(provincia);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Provincia provincia)
+        {
+            _context.Update(provincia);
+            await _context.SaveChangesAsync();
+            return Ok(provincia);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var affectedRows = await _context.Provincias
+                .Where(p => p.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (affectedRows == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
 }
